681db40a0c0b5cd3e8f65c39eabba87ba3da6ad8
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-16 19:34:49 +0200

    my first commit :) !!!

---
+++ 01-Fundamentals-Part-1/starter/script2.js
@@ -0,0 +1,11 @@
+const myCar = "golf 4";
+const myCarEngine = "1.9 tdi";
+const myCarColor = "lightGray-";
+const sentence = "myCar is lightGray golf 4 with engine 1.9 tdi.";
+console.log(sentence);
+
+const concat =
+  "myCar is " + myCarColor + " " + myCar + " with engine " + myCarEngine + ".";
+console.log(concat);
+const literal = `myCar is ${myCarColor} golf 4 with engine 1.9 tdi.`;
+console.log(literal);
