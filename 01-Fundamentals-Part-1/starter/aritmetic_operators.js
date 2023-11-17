f1051f61e9d72efdf72d998c0f622812802f54f4
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-17 14:50:43 +0200

    operators :1234:

---
+++ 01-Fundamentals-Part-1/starter/aritmetic_operators.js
@@ -0,0 +1,8 @@
+const [x, y] = [2, 8];
+console.log("parameters", x, y);
+console.log(`plus: ${x + y}`);
+console.log(`minus: ${x - y}`);
+console.log(`multiply: ${x * y}`);
+console.log(`divide: ${x / y}`);
+console.log(`modulo: ${x % y}`);
+console.log(`exponent: ${x ** y}`);
