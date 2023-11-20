d58445c7675b5a69bf3f51f9003a14b51ffdc72c
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-20 21:23:31 +0200

    calculation :1234:

---
+++ 01-Fundamentals-Part-1/starter/calculation.js
@@ -0,0 +1,15 @@
+let calculation = 0;
+console.log(calculation);
+calculation = calculation + 10;
+console.log("add 10 = " + calculation);
+calculation = calculation - 5;
+console.log("subtract 5 = " + calculation);
+calculation = calculation ** 2; // calculation*calculation  ; the same.
+console.log("exponent = " + calculation);
+calculation = calculation / 2;
+console.log("division = " + calculation);
+calculation = calculation * 3;
+console.log(`multiplication = ${calculation}`);
+console.log(`multiplication = ` + calculation);
+calculation = calculation % 5;
+console.log(`modulo = ` + calculation);
