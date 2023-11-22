c1d0d446cd5fa3de122cc4a96927cdbaa1e9dc77
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-22 21:23:46 +0200

    multi :bee:

---
+++ 01-Fundamentals-Part-1/starter/math2.js
@@ -0,0 +1,11 @@
+let a = 4.4;
+let b = -3.1;
+console.log(a, Math.floor(a), Math.ceil(a));
+console.log(b, Math.floor(b), Math.ceil(b));
+const isNotEmptyString = Boolean("");
+console.log(isNotEmptyString);
+if (isNotEmptyString) {
+  console.log("string is not empty");
+} else {
+  console.log("empty string");
+}
