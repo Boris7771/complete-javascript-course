6a928be5e734e076928f1e401310937f43d017eb
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-23 20:47:42 +0200

    truthy :whale2:

---
+++ 01-Fundamentals-Part-1/starter/truthy.js
@@ -0,0 +1,16 @@
+// Falsy
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
+if (!undefined) {
+  console.log("its true");
+} else {
+  console.log("its false");
+}
+console.log(Boolean(0));
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+console.log(Boolean(NaN));
+console.log(Boolean(""));
