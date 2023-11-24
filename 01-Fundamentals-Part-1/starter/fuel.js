141ebca345cf223243e1e80db1d721feb01f7889
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-24 21:59:31 +0200

    fuel station :truck:

--- 01-Fundamentals-Part-1/starter/fuel.js
+++ 01-Fundamentals-Part-1/starter/fuel.js
@@ -1,6 +1,6 @@
-const reservoir = 45;
+const reservoir = 30;
 let availability = 5;
-let litersPer100Km = 2;
+const litersPer100Km = 2;
 const destinationKm = 1000;
 const litersToDestination = (litersPer100Km * destinationKm) / 100;
 console.log(Math.ceil(litersToDestination), "needed for the trip");
@@ -9,4 +9,17 @@ if (reservoir > litersNeeded) {
   console.log(litersNeeded - availability, "liters on station");
 } else {
   console.log("reservoir is too small");
+}
+// fuel loaded
+const fuelAsked = 30;
+const freeSpaceInReservoir = reservoir - availability;
+console.log(freeSpaceInReservoir, "free space in reservoir");
+if (freeSpaceInReservoir < fuelAsked) {
+  availability = availability + freeSpaceInReservoir; //load fuel
+  console.log(freeSpaceInReservoir, "loaded fuel");
+  console.log(availability, "new availability");
+} else {
+  availability = availability + fuelAsked; //load fuel
+  console.log(fuelAsked, "loaded fuel");
+  console.log(availability, "new availability");
 }
