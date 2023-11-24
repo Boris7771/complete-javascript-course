8d438440e66618472f522e6791146e0fc94a3d69
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-24 22:22:07 +0200

    fuel for trip :airplane:

--- 01-Fundamentals-Part-1/starter/fuel.js
+++ 01-Fundamentals-Part-1/starter/fuel.js
@@ -1,17 +1,17 @@
-const reservoir = 30;
+const reservoir = 50;
 let availability = 5;
-const litersPer100Km = 2;
+const litersPer100Km = 5;
 const destinationKm = 1000;
 const litersToDestination = (litersPer100Km * destinationKm) / 100;
 console.log(Math.ceil(litersToDestination), "needed for the trip");
 const litersNeeded = Math.ceil(litersToDestination);
-if (reservoir > litersNeeded) {
+if (reservoir >= litersNeeded) {
   console.log(litersNeeded - availability, "liters on station");
 } else {
   console.log("reservoir is too small");
 }
 // fuel loaded
-const fuelAsked = 30;
+const fuelAsked = 45;
 const freeSpaceInReservoir = reservoir - availability;
 console.log(freeSpaceInReservoir, "free space in reservoir");
 if (freeSpaceInReservoir < fuelAsked) {
@@ -22,4 +22,10 @@ if (freeSpaceInReservoir < fuelAsked) {
   availability = availability + fuelAsked; //load fuel
   console.log(fuelAsked, "loaded fuel");
   console.log(availability, "new availability");
+}
+
+if (availability >= litersNeeded) {
+  console.log("you have enough fuel for the trip");
+} else {
+  console.log("Not enough fuel for the trip");
 }
