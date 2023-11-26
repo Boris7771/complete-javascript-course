4499fdb0ba4cd28eec4141ad0157f16b4e3a26cf
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-11-26 11:00:49 +0200

    .

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,10 +1,1 @@
-const string1 = "I'm";
-const string2 = "My job is";
-let myName = "Pencho";
-const myJob = "coder";
-const concat = string1 + " " + myName + ". " + string2 + " " + myJob + ".";
-
-console.warn(concat);
-myName = "Bobby";
-const literal = `${string1} ${myName}. ${"My job is"} ${myJob}.`;
-console.log(literal);
+const favourite = prompt("What's your favourite number?");
