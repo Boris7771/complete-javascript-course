e5c3dd9370373de6fb03c4efc2a6e820c1fa36dd
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-12-02 16:27:57 +0200

    literals :joy:

---
+++ 01-Fundamentals-Part-1/starter/literals.js
@@ -0,0 +1,36 @@
+/* // num
+1
+// NaN
+NaN
+// boolean
+true or false
+// string
+'Hello World'
+// null
+null
+// Symbol
+Symbol(JS)
+// БigInt
+BigInt(76324234274527842)
+// undefiined
+undefined */
+console.log(34);
+console.log(Number(34));
+console.log(NaN);
+console.log(String("\"Boris\", 'Borisov', `Naydenov`"));
+console.log(String("\"Boris\", 'Borisov', `Naydenov`"));
+console.log("\"Boris\", 'Borisov', `Naydenov`");
+console.log(String("Boris"));
+console.log(Symbol(1));
+console.log(BigInt(28563523));
+console.log(undefined);
+console.log(true);
+console.log(Boolean("0"));
+console.log(false);
+console.log(Boolean(0));
+console.log(Boolean([]));
+console.log(Boolean({}));
+console.log(null);
+let name = "'Bobi'";
+name = name + "\tNaydenov";
+console.log(name);
