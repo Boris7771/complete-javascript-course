eb673e00a9326aad5531e9049c557d520f28fd8b
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-12-06 21:42:11 +0200

    Array_salution :hourglass_flowing_sand:

---
+++ 01-Fundamentals-Part-1/starter/array3.js
@@ -0,0 +1,30 @@
+const pork = [50, 60, 70];
+let firstValue = pork[1];
+console.log(firstValue);
+pork[1] = "333";
+console.log(pork);
+console.log(pork[1]);
+let str = "hipermax";
+str[2] = "b";
+console.log(str[2]);
+str = "hibermax";
+console.log(str);
+
+const megaArray = [[23], [33], [727, "Baba"], "falafel", 7];
+console.log(megaArray[1]);
+megaArray[1] = ["Kebabche", "Burger"];
+console.log(megaArray);
+console.log(megaArray[1][0]);
+
+const myArray = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+  [[10, 11, 12], 13, 14],
+];
+
+const myData = myArray[3][0][2];
+console.log(myData);
+
+const botle = "  coca-cola";
+console.log(botle.toLocaleUpperCase());
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,1 +1,11 @@
-const favourite = prompt("What's your favourite number?");
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 3) {
+  console.log("Cool! 3 is an amazing numer!");
+} else if (fevourite === 7) {
+  console.log("7 is also a cool numer");
+} else {
+  console.log("Number is not 3 or 7");
+}
