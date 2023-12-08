abeb6f2040e66bcf87cd9f393e528e2fac8f42b0
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2023-12-08 21:59:36 +0200

    function arguments :snail:

---
+++ 01-Fundamentals-Part-1/starter/function_parameters.js
@@ -0,0 +1,13 @@
+function operations(x, y) {
+  console.log("this are numbers", x, y);
+  console.log("sum", x + y);
+  console.log("minus", x - y);
+  console.log("multiply", x * y);
+  console.log("divide", x / y);
+  console.log("modulo", x % y);
+  console.log("exponent", x ** y);
+}
+operations(1, 0);
+operations(4, 6);
+operations(2, 500);
+operations(2000, 2);
