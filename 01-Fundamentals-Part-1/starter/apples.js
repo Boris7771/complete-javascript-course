101540215e98796eac01403774b14c22c596c028
Boris7771 <149595217+Boris7771@users.noreply.github.com>
2024-02-11 14:14:09 +0200

    project 🔠

--- 01-Fundamentals-Part-1/starter/apples.js
+++ 01-Fundamentals-Part-1/starter/apples.js
@@ -1,6 +1,6 @@
 function apples(name, qty) {
   console.log(qty > 1 ? "aplles" : "apple");
-  return name + " eat " + (qty > 1 ? "aplles" : "apple");
+  return name + " eats " + (qty > 1 ? "aplles" : "apple");
 }
 
 console.log(apples("Grisho", 6));
---
+++ 01-Fundamentals-Part-1/starter/calculate_price.js
@@ -0,0 +1,7 @@
+function calculate(qty, price) {
+  console.log((qty * price).toFixed(2));
+}
+
+calculate(3, 2.4);
+calculate(5, 4.99);
+calculate(6, 2.0);
